40ff0c3bb555e798a4480c355b674339d1fab559
taisuke0804 <ultrasoul1987@yahoo.co.jp>
2020-08-26 18:54:28 +0900

    index.html.erbの修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,7 +36,7 @@ class CalendarsController < ApplicationController
       end
       wday_num = Date.today.wday
       if  wday_num >= 7
-        wday_num = wday_num - 7
+        wday_num = @wday_num - 7
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, week_day_char: wdays[wday_num] }
       @week_days.push(days)
